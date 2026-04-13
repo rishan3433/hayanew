a5631ca7c65a55587bb51d2d6fe15236576fb596
Rishanahmed1 <rishanahmedofficial@gmail.com>
2026-04-13 21:36:03 +0530

    push

--- javascript/script.js
+++ javascript/script.js
@@ -13,3 +13,25 @@ function CloseSide() {
     const btn = document.querySelector(".clickbtn")
     btn.style.display='block'
 }   
+
+var counter = 1;
+
+setInterval(() => {
+    var Opened = document.querySelector("#slide-" + counter);
+    var radio_open = document.querySelector(".slide-r-" + counter);
+    counter++;
+    var radio_close = document.querySelector(".slide-r-" + counter);
+    var closed = document.querySelector("#slide-" + counter);
+
+    if (counter == 7) {
+        counter = 1;
+        closed = document.querySelector("#slide-" + counter);
+        radio_close = document.querySelector(".slide-r-" + counter);
+
+    }
+
+    Opened.style.display = 'none';
+    closed.style.display = 'block';
+    radio_open.checked = false;
+    radio_close.checked = true;
+}, 2000);
