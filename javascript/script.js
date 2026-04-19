2e9092559d6a61c24c2d2021b2c71b3ca8280b74
Rishanahmed1 <rishanahmedofficial@gmail.com>
2026-04-19 19:18:59 +0530

    h

--- javascript/script.js
+++ javascript/script.js
@@ -18,20 +18,15 @@ var counter = 1;
 
 setInterval(() => {
     var Opened = document.querySelector("#slide-" + counter);
-    var radio_open = document.querySelector(".slide-r-" + counter);
     counter++;
-    var radio_close = document.querySelector(".slide-r-" + counter);
     var closed = document.querySelector("#slide-" + counter);
 
     if (counter == 7) {
         counter = 1;
         closed = document.querySelector("#slide-" + counter);
-        radio_close = document.querySelector(".slide-r-" + counter);
 
     }
 
     Opened.style.display = 'none';
     closed.style.display = 'block';
-    radio_open.checked = false;
-    radio_close.checked = true;
 }, 2000);
