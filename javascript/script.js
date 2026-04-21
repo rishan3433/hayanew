f84737043c954648174c212e9b5c0e64ee1b7bdf
Rishanahmed1 <rishanahmedofficial@gmail.com>
2026-04-21 22:28:13 +0530

    edit

--- javascript/script.js
+++ javascript/script.js
@@ -3,16 +3,16 @@ function ShowSide() {
     Side.style.display = 'block'
 
     const btn = document.querySelector(".clickbtn")
-    btn.style.display='none'
-}   
+    btn.style.display = 'none'
+}
 
 function CloseSide() {
     const Side = document.querySelector(".nav-ul")
     Side.style.display = 'none'
 
     const btn = document.querySelector(".clickbtn")
-    btn.style.display='block'
-}   
+    btn.style.display = 'block'
+}
 
 var counter = 1;
 
@@ -30,3 +30,50 @@ setInterval(() => {
     Opened.style.display = 'none';
     closed.style.display = 'block';
 }, 2000);
+
+var brandcount = -2;
+var brandup = false;
+
+setInterval(() => {
+    var w = window.innerWidth;
+
+    const img = document.getElementById("LM1-P");
+
+
+    if (w > 850) {
+        if (brandcount < -125) {
+            brandup = true;
+        }
+        if (brandcount > 0) {
+            brandup = false
+        }
+
+        if (brandup) {
+            brandcount = brandcount - brandcount;
+            brandcount = brandcount + 123
+
+        } else {
+            brandcount = brandcount - 0.5
+        }
+    }
+
+    if (w < 850) {
+        if (brandcount < -45) {
+            brandup = true;
+        }
+        if (brandcount > 0) {
+            brandup = false
+        }
+
+        if (brandup) {
+            brandcount = brandcount - brandcount;
+            brandcount = brandcount + 43
+
+        } else {
+            brandcount = brandcount - 0.5
+        }
+    }
+
+    img.style.marginLeft = brandcount + "vh";
+
+}, 50);
