03d432c22a68445a0b2f48af6853872e39bb212a
Rishanahmed1 <rishanahmedofficial@gmail.com>
2026-05-05 16:24:59 +0530

    h

--- javascript/contact-us.js
+++ javascript/contact-us.js
@@ -13,23 +13,3 @@ function CloseSide() {
     const btn = document.querySelector(".clickbtn")
     btn.style.display = 'block'
 }
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyC7wDbZ7i-HAT5GHQB8jp2nRRn3E6xbO-w",
-    authDomain: "hayawebsite123.firebaseapp.com",
-    projectId: "hayawebsite123",
-    storageBucket: "hayawebsite123.firebasestorage.app",
-    messagingSenderId: "923917095943",
-    appId: "1:923917095943:web:9fddda0c7df2f09afa06a2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = firebaseConfig.firestore();
-
